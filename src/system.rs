97560a8f2e3250ed5efb106b8a2ba31767b6ca1d
benedict <yappy.benedict@gmail.com>
2025-12-13 09:17:52 +0700

    print systemd user unit: dynamic executable

--- src/system.rs
+++ src/system.rs
@@ -1,12 +1,26 @@
-pub fn get_systemd_unit() -> &'static str {
-    r#"[Unit]
-Description=RAM Sentinel - Userspace OOM Prevention Daemon
-Documentation=https://github.com/benedict/ram-sentinel
-After=graphical-session.target.target
+use std::env;
+use std::path::PathBuf;
+
+pub fn get_systemd_unit() -> String {
+    let path_result: Result<PathBuf, std::io::Error> = env::current_exe();
+
+    let exec_start_path = match path_result {
+        Ok(path_buf) => path_buf.to_string_lossy().into_owned(),
+        Err(_e) => {
+            let fallback_path: &str =
+                "/usr/local/bin/ram-sentinel # Ensure ensure this path is correct";
+            fallback_path.to_owned()
+        }
+    };
+    let unit_file_content: String = format!(
+        r#"[Unit]
+Description=RAM Sentinel - OOM Prevention Daemon
+Documentation=https://github.com/benedictjohannes/ram-sentinel
+After=graphical-session.target
 
 [Service]
 Type=simple
-ExecStart=%h/.cargo/bin/ram-sentinel
+{}
 Restart=on-failure
 RestartSec=5s
 Nice=-10
@@ -14,5 +28,8 @@ OOMScoreAdjust=-1000
 
 [Install]
 WantedBy=default.target
-"#
+"#,
+        format!("ExecStart={}", exec_start_path),
+    );
+    unit_file_content
 }
